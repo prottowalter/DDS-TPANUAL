b31a5db8b341ef4755bc89690291f285a1db9c94
dipatata <aledbarreto@gmail.com>
2016-06-29 20:48:27 -0300

    Construyendo Direccion 1

--- src/main/java/tpanual/main/Direccion.java
+++ src/main/java/tpanual/main/Direccion.java
@@ -1,5 +1,94 @@
 package tpanual.main;
 
 public class Direccion {
-
+	private String callePrincipal;
+	private String entreCalle1;
+	private String entreCalle2;
+	private String numero;
+	private String piso;
+	private String departamento;
+	private String unidad;
+	private String codigoPostal;
+	private String barrio;
+	private String localidad;
+	private String provincia;
+	private String pais;
+	
+	public Direccion(String callePrincipal, String entreCalle1, String entreCalle2, String numero, String piso, String departamento, String unidad, 
+			String codigoPostal, String barrio, String localidad, String provincia, String pais){
+		
+	}
+	
+	public String getCallePrincipal() {
+		return callePrincipal;
+	}
+	public void setCallePrincipal(String callePrincipal) {
+		this.callePrincipal = callePrincipal;
+	}
+	public String getEntreCalle1() {
+		return entreCalle1;
+	}
+	public void setEntreCalle1(String entreCalle1) {
+		this.entreCalle1 = entreCalle1;
+	}
+	public String getEntreCalle2() {
+		return entreCalle2;
+	}
+	public void setEntreCalle2(String entreCalle2) {
+		this.entreCalle2 = entreCalle2;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getPiso() {
+		return piso;
+	}
+	public void setPiso(String piso) {
+		this.piso = piso;
+	}
+	public String getDepartamento() {
+		return departamento;
+	}
+	public void setDepartamento(String departamento) {
+		this.departamento = departamento;
+	}
+	public String getUnidad() {
+		return unidad;
+	}
+	public void setUnidad(String unidad) {
+		this.unidad = unidad;
+	}
+	public String getCodigoPostal() {
+		return codigoPostal;
+	}
+	public void setCodigoPostal(String codigoPostal) {
+		this.codigoPostal = codigoPostal;
+	}
+	public String getBarrio() {
+		return barrio;
+	}
+	public void setBarrio(String barrio) {
+		this.barrio = barrio;
+	}
+	public String getLocalidad() {
+		return localidad;
+	}
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+	public String getProvincia() {
+		return provincia;
+	}
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+	public String getPais() {
+		return pais;
+	}
+	public void setPais(String pais) {
+		this.pais = pais;
+	}
 }
