1ac724f49b19490c8dafe7a7264f6fe3b523ab98
dipatata <aledbarreto@gmail.com>
2016-06-29 23:31:38 -0300

    Agrego el metodo para buscar el colectivo

--- src/test/java/tpanual/test/MapaTest.java
+++ src/test/java/tpanual/test/MapaTest.java
@@ -2,6 +2,7 @@ package tpanual.test;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.junit.Test;
@@ -23,7 +24,16 @@ public class MapaTest {
 	
 	@Test
 	public void buscarParadaDeColectivoTest(){
-
+		Mapa mapa=getMapaConPuntosDeInteres();
+		String criterio="114";
+		List<PuntoDeInteres> lista=mapa.obtenerPuntosDeInteres(criterio);
+		Iterator<PuntoDeInteres> i = lista.iterator();
+		while(i.hasNext()){
+			if (i.next().getNombre().equals("Para de la linea ciento catorce")){
+				assertTrue(true);
+			}
+		}
+		assertFalse(true);
 		
 	}
 	
@@ -34,14 +44,21 @@ public class MapaTest {
 	
 	public Mapa getMapaConPuntosDeInteres(){
 		Mapa mapa=Mapa.getInstance();
+		
 		Direccion direccion=new Direccion.DireccionBuilder().callePrincipal("Pueyrredon").numero("545").barrio("Once").codigoPostal("1701").pais("Argentina")
 		.provincia("Ciudad de Buenos Aires").crearDireccion();
 		List<String> palabras=new ArrayList<String>();
 		palabras.add("CGP");
 		palabras.add("Zona peligrosa");
 		List<Servicio> servicios=Servicio.getListaServicios("Registro Civil", "Denuncias", "Pensiones");
+
 		PuntoDeInteres pdi=PuntoDeInteresFactory.getCGP(2500D, 3200D, "GCP Comuna 1", direccion, palabras, servicios, 25);
+		PuntoDeInteres pdi2=PuntoDeInteresFactory.getParadaDeColectivo(600, 1200, "Para de la linea ciento catorce", direccion, palabras, "114");
+		
+		
 		mapa.agregarPunto(pdi);
+		mapa.agregarPunto(pdi2);
+		
 		return mapa;
 	}
 
