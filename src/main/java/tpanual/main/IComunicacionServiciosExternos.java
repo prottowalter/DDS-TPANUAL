b7726e59ea13fe3d04a9aa848ee73ca7bff4a035
millanes <millanes92@gmail.com>
2016-07-29 17:40:46 -0300

    Corrección de la descripción de los metodos en la interface

--- src/main/java/tpanual/main/IComunicacionServiciosExternos.java
+++ src/main/java/tpanual/main/IComunicacionServiciosExternos.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public interface IComunicacionServiciosExternos {
 	/**
-	 * Recibe nombre de una calle o de una zona y devuelve la lista de CGPs que matchean, según el servicio REST correspondiente.
+	 * Recibe nombre de una calle o de una zona y devuelve la lista de CGPs que matchean, segun el servicio REST correspondiente.
 	 * ATENCION: Respuesta lenta
 	 * @param
 	 * @return Lista de CGP (puntos de interes)
@@ -12,7 +12,7 @@ public interface IComunicacionServiciosExternos {
 	public List<PuntoDeInteres> obtenerCGPEnCalleOZona (String calleOZona);
 	
 	/**
-	 * Recibe nombre de banco, algún servicio ofrecido por el mismo	y devuelve la lista de Bancos que matchean, segun el servicio REST correspondiente.
+	 * Recibe nombre de banco, algun servicio ofrecido por el mismo	y devuelve la lista de Bancos que matchean con ambos, segun el servicio REST correspondiente.
 	 * ATENCION: Servicio pago por cantidad de pegadas
 	 * @param
 	 * @return Lista de Bancos (puntos de interes)
