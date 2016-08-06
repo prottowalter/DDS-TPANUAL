02467aa856a02d6d0557ee9385b8063475b860f7
SebastianRosenberg <rosenberg.sebastian@gmail.com>
2016-08-06 13:30:20 -0300

    Clase AdministradorDeBusaquedaTest subo correcciones de espacios y lineas sin uso

--- src/test/java/tpanual/test/AdministradorDeBusquedasTest.java
+++ src/test/java/tpanual/test/AdministradorDeBusquedasTest.java
@@ -20,25 +20,23 @@ public class AdministradorDeBusquedasTest {
 	public void administradorDeBusquedasDeBancoTest() {
 		// TODO Auto-generated constructor stub
 		
-				//AdministradorDeBusquedas busqueda = AdministradorDeBusquedas.getInstance();
+		//AdministradorDeBusquedas busqueda = AdministradorDeBusquedas.getInstance();
 		AdministradorDePoi punto = new AdministradorDePoi();
 				
-				//Creo la dirección
-				Direccion direccionDeLaSucursal= new Direccion.DireccionBuilder().barrio("Villa Urquiza").callePrincipal("Av. Triunvirato").numero("5201").crearDireccion();
-				ArrayList<String> palabrasClave = new ArrayList<String>();
-				palabrasClave.add("Nunca tiene plata");
-				List<Servicio> servicios=Servicio.getListaServicios("Depositos", "Extracciones");
-				
-				PuntoDeInteres puntoFactory = PuntoDeInteresFactory.getSucursal(-34.573001D, -58.490937D, "Banco Frances", direccionDeLaSucursal, palabrasClave, servicios);
-				
-				
-				punto.agregarPoi(puntoFactory);
-				
-				List<PuntoDeInteres> listaResultado = punto.busquedaDePuntosDeInteres("Banco Frances");
-				
-				
-				assertTrue( listaResultado.size() > 0);
-				assertFalse( listaResultado.size() == 0);
+		//Creo la dirección
+		Direccion direccionDeLaSucursal= new Direccion.DireccionBuilder().barrio("Villa Urquiza").callePrincipal("Av. Triunvirato").numero("5201").crearDireccion();
+		ArrayList<String> palabrasClave = new ArrayList<String>();
+		palabrasClave.add("Nunca tiene plata");
+		List<Servicio> servicios=Servicio.getListaServicios("Depositos", "Extracciones");
+			
+		PuntoDeInteres puntoFactory = PuntoDeInteresFactory.getSucursal(-34.573001D, -58.490937D, "Banco Frances", direccionDeLaSucursal, palabrasClave, servicios);
+						
+		punto.agregarPoi(puntoFactory);
+			
+		List<PuntoDeInteres> listaResultado = punto.busquedaDePuntosDeInteres("Banco Frances");
+						
+		assertTrue( listaResultado.size() > 0);
+		assertFalse( listaResultado.size() == 0);
 		
 	}
 	
@@ -47,26 +45,23 @@ public class AdministradorDeBusquedasTest {
 	public void administradorDeBusquedasDeCgpTest() {
 		// TODO Auto-generated constructor stub
 		
-				//AdministradorDeBusquedas busqueda = AdministradorDeBusquedas.getInstance();
+		//AdministradorDeBusquedas busqueda = AdministradorDeBusquedas.getInstance();
 		AdministradorDePoi punto = new AdministradorDePoi();
 				
-				//Creo la dirección
+		//Creo la dirección
 		Direccion direccionCGP = new Direccion.DireccionBuilder().barrio("Villa Urquiza").callePrincipal("Miller").numero("2751").crearDireccion();
 		ArrayList<String> palabrasClave = new ArrayList<String>();
 		palabrasClave.add("CGP");
 		List<Servicio> servicios=Servicio.getListaServicios("Registro Civil");
 			
 		PuntoDeInteres puntoFactory = PuntoDeInteresFactory.getCGP(-34.568574D, -58.482842D, "CGP 12", direccionCGP, palabrasClave, servicios, 12);
-		
+					
+		punto.agregarPoi(puntoFactory);
 				
-				punto.agregarPoi(puntoFactory);
+		List<PuntoDeInteres> listaResultado = punto.busquedaDePuntosDeInteres("");			
 				
-				List<PuntoDeInteres> listaResultado = punto.busquedaDePuntosDeInteres("CGP 12");
-				
-				
-				assertTrue( listaResultado.size() > 0);
-				assertFalse( listaResultado.size() == 0);
-		
+		assertTrue( listaResultado.size() > 0);
+		assertFalse( listaResultado.size() == 0);	
 	}
 
 }
